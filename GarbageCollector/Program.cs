07c812cf5ee255255c732aa343c7d4c84245acbc
HALLGATO <HALLGATO@I417-01>
2022-09-15 09:04:48 +0200

    Add project files.

---
+++ GarbageCollector/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace GarbageCollector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 0;
+            while(true)
+            {
+                disposeable dp = new disposeable();
+                if (max < dp.get())
+                {
+                    max = dp.get();
+                }
+                else
+                {
+                    break;
+                }
+                Console.WriteLine(dp.get());
+            }
+            Console.WriteLine("Max number of instances: {0}", max);
+        }
+    }
+    class disposeable
+    {
+        static int counter; 
+        public disposeable(){
+            add();
+        }
+        void add()
+        {
+            counter++;
+        }
+        public int get()
+        {
+            return counter;
+        }
+        ~disposeable()
+        {
+            counter--;
+        }
+    }
+}
