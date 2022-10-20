c2bf78319732816b19e6f8094e679d33ddcd7640
Rémai Gergő (GraidN) <remaigergo@gmail.com>
2022-10-20 09:52:43 +0200

    Add files via upload

---
+++ Reflex/Reflex/Model/ReflexGame.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reflex.Model
+{
+    class ReflexGame:BindableBase
+    {
+        private int _Score  = 0;
+        public int Score { get { return _Score; }
+            set { _Score = value;
+                OnPropertyChanged("Score");
+            }
+        }
+        private int ElapsedSeconds=0;
+        public string Name = "";
+        public void AddScore()
+        {
+            Score++;
+        }
+        
+    }
+}
---
+++ Reflex/Reflex/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Reflex.Model;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Reflex.ViewModels
+{
+    class MainWindowViewModel:BindableBase
+    {
+        private ICommand _ScoreCommand;
+        public ICommand ScoreCommand
+        {
+            get
+            {
+                return _ScoreCommand;
+            }
+            set
+            {
+
+                _ScoreCommand = value;
+            }
+        }
+        public ReflexGame Game;
+        public int Score
+        {
+            get { return Game.Score; }
+            set
+            {
+                Game.Score = value;
+            }
+        }
+        MainWindowViewModel()
+        {
+            Game = new ReflexGame();
+
+        }
+        private Thickness _Position = new Thickness( 0, 0, 0, 0 );
+
+        public Thickness Position
+        {
+            get { return _Position; }
+            set { _Position = value;
+                OnPropertyChanged("Position");
+            }
+        }
+        private void AddScore(string Score)
+        {
+            Debug.WriteLine("Clicked!");
+            Game.AddScore();
+        }
+    }
+}
