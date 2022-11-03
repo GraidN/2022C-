e9b3e0382ed2fd4fe2c115861cfaa63e6c7cf5f1
Rémai Gergő (GraidN) <remaigergo@gmail.com>
2022-11-03 09:26:06 +0100

    Update

--- RandomSalary/RandomSalary/Program.cs
+++ RandomSalary/RandomSalary/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
+using System.Threading;
 
 namespace RandomSalary
 {
@@ -12,40 +14,61 @@ namespace RandomSalary
         static void Main(string[] args)
         {
             GenerateRandomSalary();
-           // ReadAndUpdate();
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            ReadAndUpdate();
+            sw.Stop();
+            Console.WriteLine("Binary Update: {0} ms", sw.ElapsedMilliseconds);
+
         }
 
         private static void ReadAndUpdate()
         {
             List<int> Salaries = new List<int>();
-           var bytes = File.ReadAllBytes("RandomSalary.bin");
-            for (int i = 0; i < 100000; i++)
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            var bytes = File.ReadAllBytes("RandomSalary.bin");
+            sw.Stop();
+            for (int i = 0; i < bytes.Length / 4; i++)//No specific number
             {
-                //Salaries.Add(BinaryReader.ToInt32(bytes,0));
+                Salaries.Add(BitConverter.ToInt32(bytes, i*4));
             }
-            Console.WriteLine(Salaries[0]);
+            BinaryWriter bw = new BinaryWriter(File.Open("UpdatedRandomSalary.bin", FileMode.Create));
+            Stopwatch sw2 = new Stopwatch();
+            sw2.Start();
+            foreach (var salary in Salaries)
+            {
+                bw.Write(BitConverter.GetBytes((int)(salary * 1.2)));
+            }
+            sw2.Stop();
+
+            Console.WriteLine("Binary Read: {0} ms", sw.ElapsedMilliseconds);
+            Console.WriteLine("Binary Write&Covnert: {0} ms", sw2.ElapsedMilliseconds);
+            Console.WriteLine("Numbers in file: {0}", bytes.Length/4);
+
         }
 
         public static void GenerateRandomSalary()
         {
-            List<int> Salaries = new List<int>();
+            List<Byte[]> Salaries = new List<Byte[]>();
             Random rnd = new Random();
-            for (int i = 0; i < 100000; i++)
+            for (int i = 0; i < 1000000; i++)
             {
-                Salaries.Add(rnd.Next(200000, 5500000));
-                using (var stream = File.Open("RandomSalary.bin", FileMode.Create))
-                {
-                    using (var writer = new BinaryWriter(stream, Encoding.UTF8, false))
-                    {
-                        writer.Write(Salaries[i]);
-                    }
-                }
+                Salaries.Add(BitConverter.GetBytes(rnd.Next(200000, 550000)));
             }
 
 
-            DateTime Startime = DateTime.Now;
-            Console.WriteLine(Salaries[0]);
-            Console.WriteLine("Binary Write: {0} ms", DateTime.Now.Millisecond - Startime.Millisecond);
+            BinaryWriter bw = new BinaryWriter(File.Open("RandomSalary.bin", FileMode.Create));
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            foreach (Byte[] values in Salaries)
+            {
+                bw.Write(values);
+            }
+            sw.Stop();
+            bw.Close();
+
+            Console.WriteLine("Binary Write: {0} ms", sw.ElapsedMilliseconds);
         }
     }
 }
